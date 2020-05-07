1f87e43eb42fc06c1bce7023c1fc53b9bbe7b4d1
Nathan Ward <wardnath.alt@gmail.com>
2020-05-07 08:43:52 -0400

    Create Form

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Weather App</h1>
+      <h2>Enter location for 5 day weather forecast</h2>
+      <form>
+        <label>
+          Location
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
