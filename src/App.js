aff4a037aaf87638ce4a3918a56c6de8d3b3fdb7
Nathan Ward <wardnath.alt@gmail.com>
2020-05-07 09:00:36 -0400

    form with state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <h2>Enter location for 5 day weather forecast</h2>
-      <form>
-        <label>
-          Location
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        <h2>Enter location for 5 day weather forecast</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Location:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
+
+export default App;
